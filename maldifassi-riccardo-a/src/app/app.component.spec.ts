48aef8e044d65aa71a039ece1dada8e34c6aa071
Riccardo Maldifassi <maldifassi.riccardo@iisgalvanimi.edu.it>
2025-02-26 12:56:29 +0000

    fine punto 4

--- maldifassi-riccardo-a/src/app/app.component.spec.ts
+++ maldifassi-riccardo-a/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'maldifassi-riccardo-a' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('maldifassi-riccardo-a');
+    expect(app.name).toEqual('maldifassi-riccardo-a');
   });
 
   it('should render title', () => {
